661226dded362d4b0fd38f39c4104ecf7bd0cf5f
miirinjej <miirinjej@gmail.com>
2020-03-23 15:40:13 +0500

    Feat: add an animated cube
    Create a separate component and import it into the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Canvas } from 'react-three-fiber';
 import './App.scss';
+import Cube from './components/3d/Cube';
 
 function App() {
   return (
     <div>
       Three.js manual.
+      <Canvas
+        camera={{
+          fov: 75,
+          near: 0.01,
+          far: 1000,
+        }}
+      >
+        <Cube />
+      </Canvas>
     </div>
   );
 }
